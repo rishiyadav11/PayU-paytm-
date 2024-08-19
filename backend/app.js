0c27f962f27d46f799f623c01e77cb35186e3ad5
rishiyadav11 <photonics54yadav@gmail.com>
2024-08-19 21:28:33 +0530

    edit cors again

--- backend/app.js
+++ backend/app.js
@@ -15,13 +15,23 @@ mongooseconnect();
 
 // Middleware
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin',process.env.FRONTEND_URL);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(cors({
     origin: process.env.FRONTEND_URL, // Allow only your frontend origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
     credentials: true // If you need to send cookies or other credentials
 }));
-app.options('*', cors()); // Preflight all routes
-
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.sendStatus(200);
+  });
+  
 app.use(bodyParser.json());
 app.use(express.json());
 
